ac555b2df1c99f1438e076fe9ab19329cdfe5bfa
James Haller <jameshaller27@gmail.com>
2021-04-13 23:56:22 -0700

    Reformat output + update example

--- treeify.py
+++ treeify.py
@@ -1,8 +1,8 @@
-EMPTY_INDENT = '    '
-PIPED_INDENT = '|   '
+EMPTY_INDENT = '   '
+PIPED_INDENT = '|  '
 SPLIT_INDENT = '├─'
 ELBOW_INDENT = '└─'
-ARRAY_INDEX =  '──┐'
+ARRAY_INDEX =  '─┐'
 
 
 class INode:
@@ -73,10 +73,15 @@ root.add_node("alpha", alpha)
 for i in range(1, 4):
     alpha.add_node(PrimitiveNode(f"alpha.{i}"))
 
+alpha4 = ListNode()
+alpha.add_node(alpha4)
+for i in range(1, 4):
+    alpha4.add_node(PrimitiveNode(f"alpha.4.{i}"))
+
 bravo = DictNode()
 root.add_node("bravo", bravo)
 for i in range(1, 4):
-    bravo.add_node(f"{i}", PrimitiveNode(f"bravo.{i}"))
+    bravo.add_node(f"k{i}", PrimitiveNode(f"bravo.{i}"))
 
 charlie = PrimitiveNode("charlie")
 root.add_node("charlie", charlie)
