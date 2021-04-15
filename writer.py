db99cde4c63f64b928e1b2a21b7e4f4413f01bf2
James <jameshaller27@gmail.com>
2021-04-15 11:07:37 -0700

    Implement primitive + dict node writing

--- writer.py
+++ writer.py
@@ -9,14 +9,42 @@ ARRAY_INDEX =  '─┐'
 
 
 class UnicodeWriter(IWriter):
+    def __init__(self):
+        self._prefixes = []
+
+    def _push_prefix(self, addition: str):
+        prefix = self._prefix + addition
+        self._prefixes.append(prefix)
+
+    def _pop_prefix(self) -> str:
+        return self._prefixes.pop()
+
+    @property
+    def _prefix(self) -> str:
+        if len(self._prefixes) == 0:
+            return ""
+
+        return self._prefixes[-1]
+
     def write_primitive(self, node: PrimitiveNode):
-        pass
+        print(self._prefix, node.value, sep="")
 
     def write_list(self, node: ListNode):
         pass
 
     def write_dict(self, node: DictNode):
-        pass
+        def _write(key: str, val: INode, split: str, indent: str):
+            print(self._prefix, split, k, sep="")
+            self._push_prefix(indent)
+            v.write_to(self)
+            self._pop_prefix()
+
+        children = node.values()
+        for k, v in children[:-1]:
+            _write(k, v, SPLIT_INDENT, PIPED_INDENT)
+
+        k, v = children[-1]
+        _write(k, v, ELBOW_INDENT, EMPTY_INDENT)
 
     def flush(self):
         pass
